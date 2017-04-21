8351bbe531f5d5cc308708a5f23f583acbc02b2c
zidong.wang <zidong.wang@qunar.com>
2017-04-21 12:47:44 +0800

    change port

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var ctrls = require('./ctrl/index');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3333);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine("html",doT.__express);
